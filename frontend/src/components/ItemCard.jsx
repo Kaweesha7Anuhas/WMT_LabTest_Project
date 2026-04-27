49bac40e4bf8df832d333d69c4c987bc797e7a5c
Kaweesha Anuhas <kaweesha7anuhas@gmail.com>
2026-04-27 11:10:29 +0530

    Update ItemCard.jsx

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -12,7 +12,7 @@ function ItemCard({ item, onDelete }) {
       <p><strong>Category:</strong> {item.category}</p>
       <p><strong>Price:</strong> ${item.price}</p>
       <p>{item.description}</p>
-
+      <p>{item.expiryDate}</p>
       <div className="card-actions">
         <Link className="btn secondary" to={`/edit-item/${item._id}`}>Edit</Link>
         <button className="btn danger" onClick={() => onDelete(item._id)}>
